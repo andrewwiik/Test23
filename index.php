5464ff62d154a2a1b85bb6916407f1120c216b86
Andrew Wiik <andrew.wiik@vtsd.com>
2015-08-25 04:38:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -675,65 +675,6 @@ if ($index == 0) {
 						</div>
 					</div>
 				</a>
-			</fieldset>
-                    <div class="tabs-container">
-                        <ul class="nav nav-tabs">
-                            <li class="active"><a data-toggle="tab" href="#tab-1" aria-expanded="true">Description</a></li>
-                            <li class=""><a data-toggle="tab" href="#tab-2" aria-expanded="false">More Info</a></li>
-                        </ul>
-                        <div class="tab-content">
-                            <div id="tab-1" class="tab-pane active">
-                                <div class="panel-body">
-                                   <?php	
-		}
-		if(!defined('DCRM_DESCRIPTION')) define('DCRM_DESCRIPTION', 2);
-		if (DCRM_MOREINFO == 2 || DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2)) {
-?>
-			<block>
-<?php
-			}
-			if (DCRM_MOREINFO == 2 && (DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2))) echo '<hr />';
-			if (DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2)) {
-?>
-					<p><?php echo(nl2br($pkg_assoc['Description'])); ?></p>
-<?php
-			}
-?>
-			</block>
-<?php
-		}
-		if (!empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2) {
-?>
-			<fieldset>
-			<div>
-			<?php $Multi = html_entity_decode(html_entity_decode($pkg_assoc['Multi']));
-			echo $Multi ?>
-			</div>
-			</fieldset>
-			<?php
-			if (DCRM_MOREINFO == 2) {
-?>
-					<p><?php _e('Version'); ?> <strong><?php echo($pkg_assoc['Version']); ?></strong> | <?php _e('Downloads'); ?> <strong><?php echo($pkg_assoc['DownloadTimes']); ?></strong></p>
-					<p><?php _e('Last Updated'); ?> <strong><?php echo($pkg_assoc['CreateStamp']); ?></strong></p>
-                                </div>
-                            </div>
-                            <div id="tab-2" class="tab-pane">
-                                <div class="panel-body">
-                                    <strong>Donec quam felis</strong>
-
-                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
-                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
-
-                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
-                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-
-<?php
 		}
 		$package_info = $pkg_assoc;
 		require_once('commercial.php');
@@ -941,6 +882,65 @@ if ($index == 0) {
 		}
 		if (!empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2) {
 ?>
+</fieldset>
+                    <div class="tabs-container">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#tab-1" aria-expanded="true">Description</a></li>
+                            <li class=""><a data-toggle="tab" href="#tab-2" aria-expanded="false">More Info</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div id="tab-1" class="tab-pane active">
+                                <div class="panel-body">
+                                   <?php	
+		}
+		if(!defined('DCRM_DESCRIPTION')) define('DCRM_DESCRIPTION', 2);
+		if (DCRM_MOREINFO == 2 || DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2)) {
+?>
+			<block>
+<?php
+			}
+			if (DCRM_MOREINFO == 2 && (DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2))) echo '<hr />';
+			if (DCRM_DESCRIPTION == 2 || (empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2)) {
+?>
+					<p><?php echo(nl2br($pkg_assoc['Description'])); ?></p>
+<?php
+			}
+?>
+			</block>
+<?php
+		}
+		if (!empty($pkg_assoc['Multi']) && DCRM_MULTIINFO == 2) {
+?>
+			<fieldset>
+			<div>
+			<?php $Multi = html_entity_decode(html_entity_decode($pkg_assoc['Multi']));
+			echo $Multi ?>
+			</div>
+			</fieldset>
+			<?php
+			if (DCRM_MOREINFO == 2) {
+?>
+					<p><?php _e('Version'); ?> <strong><?php echo($pkg_assoc['Version']); ?></strong> | <?php _e('Downloads'); ?> <strong><?php echo($pkg_assoc['DownloadTimes']); ?></strong></p>
+					<p><?php _e('Last Updated'); ?> <strong><?php echo($pkg_assoc['CreateStamp']); ?></strong></p>
+                                </div>
+                            </div>
+                            <div id="tab-2" class="tab-pane">
+                                <div class="panel-body">
+                                    <strong>Donec quam felis</strong>
+
+                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+<?php
 			<fieldset>
 			<div>
 			<?php $Multi = html_entity_decode(html_entity_decode($pkg_assoc['Multi']));
