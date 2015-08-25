4590eed0d3ec5230709b3c14e4c06cf771e0c15a
Andrew Wiik <andrew.wiik@vtsd.com>
2015-08-25 06:32:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1485,11 +1485,36 @@ if ($index == 0) {
                 dots: true
             });
 
-            $('.slick_demo_2').slick({
+                        $('.slick_demo_2').slick({
                 infinite: true,
-                slidesToShow: 3,
+                slidesToShow: 1,
                 slidesToScroll: 1,
                 centerMode: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
             });
 
             $('.slick_demo_3').slick({
