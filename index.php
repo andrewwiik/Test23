9e5cd725198c5c9971181f57101e9697817803dd
Andrew Wiik <andrew.wiik@vtsd.com>
2015-08-25 05:23:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1595,6 +1595,61 @@ if (defined("AUTOFILL_STATISTICS")) {
 <?php
 }
 ?>
+style>
+        .slick_demo_2 .ibox-content {
+            margin: 0 10px;
+        }
+    </style>
 
+    <script>
+        $(document).ready(function(){
+
+
+            $('.slick_demo_1').slick({
+                dots: true
+            });
+
+            $('.slick_demo_2').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+
+            $('.slick_demo_3').slick({
+                infinite: true,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear',
+                adaptiveHeight: true
+            });
+        });
+
+    </script>
 	</body>
 </html>
