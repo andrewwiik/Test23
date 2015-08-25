999f8515ef3038ea3893cf59e4a4823ca5264916
root <root@repo.andrewwiik.me>
2015-08-25 03:33:40 -0400

    test

--- system/config/autofill.inc.php
+++ system/config/autofill.inc.php
@@ -1,8 +1,21 @@
 <?php
-if (!defined("DCRM")) exit();
+if (!defined("DCRM")) {
+	exit;
+}
+define("AUTOFILL_PRE", "");
 define("AUTOFILL_NONAME", "[未命名]");
-define("AUTOFILL_MASTER", "admin");
-define("AUTOFILL_EMAIL", "cardflourish101@gmail.com");
+	define('AUTOFILL_MASTER', 'admin');
+define("AUTOFILL_FULLNAME", "");
+	define('AUTOFILL_EMAIL', 'cardflourish101@gmail.com');
+define("AUTOFILL_SITE", "");
+define("AUTOFILL_WEIBO", "");
+define("AUTOFILL_WEIBO_NAME", "");
 define("AUTOFILL_DESCRIPTION", "一个萌萌哒软件包。");
-define("AUTOFILL_SEO", "ATWiik\'s Repo");
+	define('AUTOFILL_SEO', 'ATWiik\'s Test Repo');
+define("AUTOFILL_KEYWORDS", "");
+define("AUTOFILL_PAYPAL", "");
+define("AUTOFILL_STATISTICS", "");
+define("AUTOFILL_STATISTICS_INFO", "");
+define("AUTOFILL_ADVERTISEMENT", "");
+define("AUTOFILL_DUOSHUO_KEY", "");
 ?>
